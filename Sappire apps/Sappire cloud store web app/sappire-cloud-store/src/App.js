7326d470de43225982c19f620d86f666133f2489
ahmadwael28 <ahmadwael28@gmail.com>
2022-08-16 01:38:55 +0200

    folder change and react app creation

---
+++ Sappire apps/Sappire cloud store web app/sappire-cloud-store/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//pages
+import { Authentication } from "./pages/authentication/Authentication";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Authentication />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Sappire apps/Sappire cloud store web app/sappire-cloud-store/src/pages/authentication/Authentication.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Authentication.scss";
+
+export const Authentication = () => {
+  return (
+    <div className="authentication__page-container">
+      <div className="authentication__page-overlay">
+        <div className="authentication__content">{"authentication"}</div>
+      </div>
+    </div>
+  );
+};
